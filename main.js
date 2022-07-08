48631e4d0ba8044b6fe3a8da8866dd4fee9e0b50
subodh254 <94366763+subodh254@users.noreply.github.com>
2022-07-08 15:41:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+video = "";
+
+function preload()
+{
+    video = createVideo('video.mp4');
+    video.hide();
+}
+
+function setup(){
+    canvas = createCanvas(480,380);
+    canvas.center();
+
+}
+
+function draw()
+{
+    image(video , 0 , 0 , 480 , 380);
+
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "status : detecting objects";
+}
+
+function modelLoaded(){
+    console.log("model Loaded !");
+    video.loop();
+    video.speed(1);
+    video.volume(3);
+}
